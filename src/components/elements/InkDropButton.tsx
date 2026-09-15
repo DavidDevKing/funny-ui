2c9849edee577acb9f2d526709f8b72211b1dd86
DavidDevKing <davedev.clone6@gmail.com>
2026-09-15 12:24:51 +0100

    fix(InkDropButton): fix InkDropShape and animation

--- src/components/elements/InkDropButton.tsx
+++ src/components/elements/InkDropButton.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Vector2 } from "../../behaviors/Utilities";
 
 
@@ -22,52 +22,73 @@ interface InkDropButtonProps{
  * Element that uses an inkdrop effect to change it's background on hover
  */
 function InkDropButton({color = "#772B81", hoverColor = "#D69F00", origin = "cursor", borderRadius = "100%", children} : InkDropButtonProps){
-    const [ishovered, setIsHovered] = useState<boolean>(false);
 
     const buttonRef = useRef<HTMLDivElement>(null);
     const foregroundRef = useRef<HTMLDivElement>(null);
 
 
-    const updateOrigin = (e: MouseEvent) => {
+    const updateInkDrop = (e: MouseEvent) => {
         if (!foregroundRef.current || !buttonRef.current) return;
-        let rect : DOMRect = buttonRef.current.getBoundingClientRect();
-        let center : Vector2 = new Vector2(rect.left + rect.width/2, rect.top + rect.height/2);
-        foregroundRef.current.style.translate = `${e.clientX - center.x}px ${e.clientY - center.y}px`
+        // Update the origin
+        const rect : DOMRect = buttonRef.current.getBoundingClientRect();
+        const rectVec : Vector2 = new Vector2(rect.width, rect.height);
+        const originVec : Vector2 = new Vector2(rect.left, rect.top);
+        const offset : Vector2 = new Vector2(e.clientX - originVec.x, e.clientY - originVec.y);
+        foregroundRef.current.style.translate = `${offset.x}px ${offset.y}px`;
+
+
+
+        /** Returns the smallest largerst distance from a point to the end of the boundaries of a box with the specified end point on a 2D plane */
+        const largestDistanceFromPoint = (point : Vector2, end : Vector2) : number => {
+            const center = end.divide(2);
+            const diffFromCenter = center.subtract(point);
+            let dist = point;
+            if (diffFromCenter.x > 0) dist.x = end.x - dist.x;
+            if (diffFromCenter.y > 0) dist.y = end.y - dist.y;
+            
+            return dist.magnitude(); 
+        }
+
+        // Update the scale
+        if (e.type === "pointerenter"){
+            let targetScale = largestDistanceFromPoint(offset, rectVec) * 2;
+            foregroundRef.current.style.scale = `${targetScale}`
+        }
+        else if (e.type === "pointerleave"){
+            foregroundRef.current.style.scale = "0";
+        }
     }
 
-    
+    // Add event listeners for pointerenter and pointer exit
     useEffect(() => {
         if (!buttonRef.current || !foregroundRef.current || !(origin === "cursor")) return;
-        buttonRef.current.addEventListener("pointerenter", updateOrigin);
-        buttonRef.current.addEventListener("pointerleave", updateOrigin);
+        buttonRef.current.addEventListener("pointerenter", updateInkDrop);
+        buttonRef.current.addEventListener("pointerleave", updateInkDrop);
         
         return () => {
-            buttonRef.current?.removeEventListener("pointerenter", updateOrigin);
-            buttonRef.current?.removeEventListener("pointerleave", updateOrigin);
+            buttonRef.current?.removeEventListener("pointerenter", updateInkDrop);
+            buttonRef.current?.removeEventListener("pointerleave", updateInkDrop);
         }
     }, [])
 
     return (
         <div
-        ref={buttonRef}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className={"relative group flex overflow-hidden cursor-pointer"}
-        style={{
-            borderRadius: `${borderRadius}`
+            ref={buttonRef}
+            className={"relative group flex overflow-hidden cursor-pointer"}
+            style={{
+                borderRadius: `${borderRadius}`
             }}>
             {children}
 
             <div
-            ref={foregroundRef}
-            className={"absolute inset-0 w-full h-full rounded-full -z-10 scale-0 duration-200 ease-in-out backdrop-hue-rotate-180"}
-            style={{
-                backgroundColor: `${hoverColor}`,
-                translate : `${origin === "left" ? "-50% 0" : origin === "right" ? "50% 0" : origin === "top" ? "0 -50%" : origin === "bottom" ? "0 50%" : "0"}`,
-                // If we start from the edge to grow the effect, the backdrop will have to strech double the distance
-                scale : `${ishovered ? (origin == "center" ? "1" : "2") : "0"}`,
-                }} ></div>
-            <div className={"absolute inset-0 w-full h-full -z-20"} style={{backgroundColor: `${color}`}} ></div>
+                ref={foregroundRef}
+                className={"absolute w-px h-px rounded-full -z-10 scale-0 duration-200 ease-in-out backdrop-hue-rotate-180"}
+                style={{
+                    backgroundColor: `${hoverColor}`,
+                    borderRadius : "100%",
+                    translate : `${origin === "left" ? "-50% 0" : origin === "right" ? "50% 0" : origin === "top" ? "0 -50%" : origin === "bottom" ? "0 50%" : "0"}`,
+                }} />
+            <div className={"absolute inset-0 w-full h-full -z-20"} style={{backgroundColor: `${color}`}} />
         </div>
     )
 }
