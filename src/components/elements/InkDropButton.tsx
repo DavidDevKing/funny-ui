dd2cea19b68e43978d0a327be9d580bbef04599f
DavidDevKing <davedev.clone6@gmail.com>
2026-09-15 09:16:23 +0100

    fix(InkDropButton): change style from inline-flex to flex

--- src/components/elements/InkDropButton.tsx
+++ src/components/elements/InkDropButton.tsx
@@ -52,7 +52,7 @@ function InkDropButton({color = "#772B81", hoverColor = "#D69F00", origin = "cur
         ref={buttonRef}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className={"relative group inline-flex overflow-hidden cursor-pointer"}
+        className={"relative group flex overflow-hidden cursor-pointer"}
         style={{
             borderRadius: `${borderRadius}`
             }}>
