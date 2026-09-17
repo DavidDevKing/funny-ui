7bb1fd751d7647b2db89c79d1523dc587c9211b0
DavidDevKing <davedev.clone6@gmail.com>
2026-09-17 11:04:23 +0100

    fix(InkDropButton): fix positioning and scaling for inkdrop with static positions. fix origin transition issue with inkDrop effect on pointerenter

--- src/components/elements/InkDropButton.tsx
+++ src/components/elements/InkDropButton.tsx
@@ -28,13 +28,15 @@ function InkDropButton({color = "#772B81", hoverColor = "#D69F00", origin = "cur
 
 
     const updateInkDrop = (e: MouseEvent) => {
-        if (!inkDropRef.current || !buttonRef.current) return;
+        const button = buttonRef.current;
+        const inkDrop = inkDropRef.current;
+        if (!inkDrop || !button) return;
         // Update the origin
-        const rect : DOMRect = buttonRef.current.getBoundingClientRect();
+        const rect : DOMRect = button.getBoundingClientRect();
         const rectVec : Vector2 = new Vector2(rect.width, rect.height);
         const originVec : Vector2 = new Vector2(rect.left, rect.top);
         const offset : Vector2 = new Vector2(e.clientX - originVec.x, e.clientY - originVec.y);
-        inkDropRef.current.style.translate = `${offset.x}px ${offset.y}px`;
+        if (origin === "cursor") inkDrop.style.translate = `${offset.x}px ${offset.y}px`;
 
 
 
@@ -51,23 +53,38 @@ function InkDropButton({color = "#772B81", hoverColor = "#D69F00", origin = "cur
 
         // Update the scale
         if (e.type === "pointerenter"){
-            let targetScale = largestDistanceFromPoint(offset, rectVec) * 2;
-            inkDropRef.current.style.scale = `${targetScale}`
+            if (origin === "cursor") {
+                const targetScale = largestDistanceFromPoint(offset, rectVec) * 2;
+                inkDrop.style.scale = `${targetScale}`
+            }
+            else if (origin === "top" || origin === "bottom") inkDrop.style.scale = `${rectVec.y * 2}`;
+            else if (origin === "left" || origin === "right") inkDrop.style.scale = `${rectVec.x *2}`;
+            else inkDrop.style.scale = `${Math.max(rectVec.x, rectVec.y)}`
         }
         else if (e.type === "pointerleave"){
-            inkDropRef.current.style.scale = "0";
+            inkDrop.style.scale = "0";
         }
     }
 
-    // Add event listeners for pointerenter and pointer exit
+    // Add event listeners for pointerenter and pointer exit and update the static positions
     useEffect(() => {
-        if (!buttonRef.current || !inkDropRef.current || !(origin === "cursor")) return;
-        buttonRef.current.addEventListener("pointerenter", updateInkDrop);
-        buttonRef.current.addEventListener("pointerleave", updateInkDrop);
+        const button = buttonRef.current;
+        const inkDrop = inkDropRef.current;
+        if (!button || !inkDrop) return;
+        button.addEventListener("pointerenter", updateInkDrop);
+        button.addEventListener("pointerleave", updateInkDrop);
+
+
+        // Update the static postions 
+        const rect : DOMRect = button.getBoundingClientRect();
+        const rectVec : Vector2 = new Vector2(rect.width, rect.height);
+
+        inkDrop.style.translate = `${origin === 'left' ? "0" : origin === 'right' ? rectVec.x : rectVec.x/2}px ${origin === 'top' ? "0" : origin === "bottom" ? rectVec.y : rectVec.y/2}px`;
+
         
         return () => {
-            buttonRef.current?.removeEventListener("pointerenter", updateInkDrop);
-            buttonRef.current?.removeEventListener("pointerleave", updateInkDrop);
+            button.removeEventListener("pointerenter", updateInkDrop);
+            button.removeEventListener("pointerleave", updateInkDrop);
         }
     }, [])
 
@@ -82,11 +99,10 @@ function InkDropButton({color = "#772B81", hoverColor = "#D69F00", origin = "cur
 
             <div
                 ref={inkDropRef}
-                className={"absolute w-px h-px rounded-full -z-10 scale-0 duration-200 ease-in-out backdrop-hue-rotate-180"}
+                className={"absolute w-px h-px rounded-full -z-10 scale-0 transition-[scale] duration-200 ease-in-out backdrop-hue-rotate-180"}
                 style={{
                     backgroundColor: `${hoverColor}`,
                     borderRadius : "100%",
-                    translate : `${origin === "left" ? "-50% 0" : origin === "right" ? "50% 0" : origin === "top" ? "0 -50%" : origin === "bottom" ? "0 50%" : "0"}`,
                 }} />
             <div className={"absolute inset-0 w-full h-full -z-20"} style={{backgroundColor: `${color}`}} />
         </div>
