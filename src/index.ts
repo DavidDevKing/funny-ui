b8fd6a086f71fb2c057505121568b499b1f6fd6b
DavidDevKing <davedev.clone6@gmail.com>
2026-09-21 14:16:15 +0100

    fix: add SmoothScroll to package export list

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as FloatingCharacter } from "./components/elements/FloatingChar
 export { default as FloatingCharacterLine } from "./components/elements/FloatingCharacterLine";
 export { default as BouncyAttractor } from "./components/elements/BouncyAttractor";
 export {default as InkDropButton } from "./components/elements/InkDropButton";
+export * as SmoothScroll from "./behaviors/SmoothScroll";
