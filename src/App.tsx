aecad782b02dffb2c7fc1882e577d25f8226a307
DavidDevKing <davedev.clone6@gmail.com>
2026-09-24 10:07:56 +0100

    fix(App): Update SmoothScroll import, since it is no longer the default export

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import BouncyAttractor from './components/elements/BouncyAttractor';
 import FloatingCharacterContainer from './components/layout/FloatingCharacterContainer';
 import InkDropButton from './components/elements/InkDropButton';
-import SmoothScroll from './behaviors/SmoothScroll';
+import { SmoothScroll } from './behaviors/SmoothScroll';
 
 SmoothScroll();
 
